4d3505d37817b697a56489895d348fdd98e681fd
Alan Zagi <alanzagi2007@gmail.com>
2025-01-01 21:20:27 +0700

    membuat beranda lebih baik

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,7 +1,40 @@
 <x-layout :title="$title">
     @section('page')
-        <main class="h-screen bg-yellow-300">
-            <h1 class="text-red-500 text-5xl">Halo</h1>
+        <main class="h-screen bg-yellow-300 font-poppins flex items-center justify-start">
+            <div class="px-5">
+                <div class="flex flex-col">
+                    <h1 class="text-gray-800 font-bold text-3xl md:text-4xl">Selalu dan #PastiSelera</h1>
+                    <p class="text-gray-800 font-normal text-md pr-20 max-w-lg md:text-xl lg:text-2xl">
+                        "Toko roti terbaik dengan rasa istimewa, selalu dan pasti selera untuk semua pelanggan."
+                    </p>
+                    <a href="" target="_blank" class="bg-gray-800 duration-200 self-start mt-3 rounded">
+                        <div
+                            class="bg-gray-200 active:translate-x-0 active:translate-y-0 active:bg-gray-200 border-gray-800 border-2 duration-200 px-4 py-2 -translate-x-1 -translate-y-1 hover:-translate-x-1.5 hover:-translate-y-1.5 rounded lg:px-5 lg:py-4 hover:bg-yellow-300">
+                            <span class="duration-200 flex justify-start items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#1F2937"
+                                    class="w-6 h-6">
+                                    <path
+                                        d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+                                </svg>
+                                <span
+                                    class="flex justify-start items-center font-medium opacity-80 hover:opacity-100 lg:uppercase lg:font-bold ">Pesan
+                                    Sekarang</span>
+                            </span>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </main>
+
+        {{-- <section class="grid grid-cols-2 bg-yellow-300">
+            <img src="{{ asset('images/black_forest_cake.jpg') }}" alt="Black Forest Cake"
+                class="w-40 rounded-full col-span-2 mx-auto">
+            <img src="{{ asset('images/croissant.jpg') }}" alt="Croissant" class="w-40 rounded-full mx-auto">
+            <img src="{{ asset('images/donut.jpg') }}" alt="Donut" class="w-40 rounded-full mx-auto">
+        </section> --}}
+
+        <section class="bg-yellow-300 font-poppins">
+
+        </section>
     @endsection
 </x-layout>
--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,12 +1,14 @@
 <nav :title="$title" class="relative">
-    <div class="absolute top-0 left-0 w-full bg-yellow-300 border-gray-200 font-poppins">
+    <div class="absolute top-0 left-0 w-full bg-yellow-300 border-gray-200 font-poppins z-10">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap">LANBAKERY</span>
+            <a href="{{ route('beranda') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="{{ asset('images/alanxcode.jpg') }}"
+                    class="h-8 rounded-full ring-2 ring-gray-800 hover:ring-gray-200" />
+                <span
+                    class="self-center text-2xl font-semibold whitespace-nowrap font-rubik-vinyl text-gray-800 hover:text-white hover:transition hover:duration-100">LANBAKERY</span>
             </a>
             <button data-collapse-toggle="navbar-default" type="button"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-800 ring-gray-800 ring-2 outline-none rounded-lg md:hidden hover:bg-gray-200 focus:bg-gray-200"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-800 ring-gray-800 ring-2 outline-none rounded-lg lg:hidden hover:bg-gray-200 focus:bg-gray-200"
                 aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -15,39 +17,51 @@
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                {{-- Tambahkan nanti ya cta buat mobile --}}
+            <div class="hidden w-full lg:block lg:w-auto" id="navbar-default">
                 <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border-2 border-gray-800 rounded-xl bg-gray-200 md:flex-row md:space-x-2 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-yellow-300">
+                    class="font-medium flex flex-col p-4 lg:p-0 mt-4 border-2 border-gray-800 rounded-xl bg-gray-200 lg:flex-row lg:space-x-2 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-yellow-300">
                     <li>
                         <a href="{{ route('beranda') }}"
-                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 md:px-5 md:py-2.5 md:ring-2 md:ring-gray-800 md:rounded-xl 
+                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 lg:px-5 lg:py-2.5 lg:ring-2 lg:ring-gray-800 lg:rounded-xl 
                                    {{ $title == 'Beranda' ? 'bg-gray-800 text-yellow-300' : 'bg-transparent' }}">
                             Beranda
                         </a>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 md:px-5 md:py-2.5 md:ring-2 md:ring-gray-800 md:rounded-xl 
+                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 lg:px-5 lg:py-2.5 lg:ring-2 lg:ring-gray-800 lg:rounded-xl 
                                    {{ $title == 'Tentang' ? 'bg-gray-800 text-yellow-300' : 'bg-transparent' }}">
                             Tentang
                         </a>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 md:px-5 md:py-2.5 md:ring-2 md:ring-gray-800 md:rounded-xl 
+                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 lg:px-5 lg:py-2.5 lg:ring-2 lg:ring-gray-800 lg:rounded-xl 
                                    {{ $title == 'Layanan' ? 'bg-gray-800 text-yellow-300' : 'bg-transparent' }}">
                             Layanan
                         </a>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 md:px-5 md:py-2.5 md:ring-2 md:ring-gray-800 md:rounded-xl 
+                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 lg:px-5 lg:py-2.5 lg:ring-2 lg:ring-gray-800 lg:rounded-xl 
                                    {{ $title == 'Kontak' ? 'bg-gray-800 text-yellow-300' : 'bg-transparent' }}">
                             Kontak
                         </a>
                     </li>
-
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 lg:px-5 lg:py-2.5 lg:ring-2 lg:ring-gray-800 lg:rounded-xl 
+                                   {{ $title == 'Toko Kami' ? 'bg-gray-800 text-yellow-300' : 'bg-transparent' }}">
+                            Toko Kami
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-3 rounded text-gray-800 hover:text-yellow-300 hover:bg-gray-800 lg:px-5 lg:py-2.5 lg:ring-2 lg:ring-gray-800 lg:rounded-xl 
+                                   {{ $title == 'Kontak' ? 'bg-gray-800 text-yellow-300' : 'bg-transparent' }}">
+                            Keranjang
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
